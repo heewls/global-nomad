a1fd3e7bae1aed6651c2c3442caaa2f3442879b9
heewls <heejin0857@naver.com>
2025-12-10 19:25:59 +0900

    feat: arrow component

---
+++ src/common/arrow/index.tsx
@@ -0,0 +1,18 @@
+import ArrowDown from "@/assets/icons/fullDownArrow.svg";
+import ArrowUp from "@/assets/icons/fullUpArrow.svg";
+
+export default function Arrow({
+  isOpen,
+  size = "24",
+  fillColor = "#1f1f22",
+}: {
+  isOpen: boolean;
+  size?: string;
+  fillColor?: string;
+}) {
+  return isOpen ? (
+    <ArrowUp width={size} height={size} fill={fillColor} />
+  ) : (
+    <ArrowDown width={size} height={size} fill={fillColor} />
+  );
+}
