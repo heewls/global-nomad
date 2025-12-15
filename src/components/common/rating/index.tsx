dadd99566a7721d0de9211097f3b3da4daed0d51
heewls <heejin0857@naver.com>
2025-12-15 21:22:47 +0900

    feat: add gap props & rename hover state

--- src/components/common/rating/index.tsx
+++ src/components/common/rating/index.tsx
@@ -8,6 +8,7 @@ interface RatingProps {
   starLength: number;
   starSize: string;
   count?: number;
+  gap?: number;
   onChange?: (star: number) => void;
   readonly?: boolean;
 }
@@ -16,10 +17,11 @@ export default function Rating({
   starLength,
   starSize = "16",
   count,
+  gap,
   onChange,
   readonly,
 }: RatingProps) {
-  const [hover, setHover] = useState<number>(0);
+  const [mouseEvent, setMouseEvent] = useState<number>(0);
   const [rating, setRating] = useState<number>(count ?? 0);
 
   const handleStarClick = (star: number) => {
@@ -31,30 +33,39 @@ export default function Rating({
 
   const handleMouseOver = (star: number) => {
     if (readonly) return;
-    setHover(star);
+    setMouseEvent(star);
   };
 
   const handleMouseLeave = () => {
     if (readonly) return;
-    setHover(0);
+    setMouseEvent(0);
   };
-
-  const activeRating = hover > 0 ? hover : rating;
 
   return (
     <div className="flex justify-center items-center">
-      {Array.from({ length: starLength }).map((_, idx) => (
-        <Star
-          className={clsx(readonly ?? "cursor-pointer")}
-          key={idx}
-          width={starSize}
-          height={starSize}
-          fill={activeRating > idx ? "#FFCB02" : "var(--color-gray100)"}
-          onClick={() => handleStarClick(idx + 1)}
-          onMouseOver={() => handleMouseOver(idx + 1)}
-          onMouseLeave={handleMouseLeave}
-        />
-      ))}
+      {Array.from({ length: starLength }).map((_, idx) => {
+        const activeRating = mouseEvent > 0 ? mouseEvent : rating;
+
+        return (
+          <div
+            key={idx}
+            className={clsx(
+              "inline-block",
+              readonly ? "cursor-default" : "cursor-pointer"
+            )}
+            style={idx < starLength - 1 ? { paddingRight: `${gap}px` } : {}}
+            onClick={() => handleStarClick(idx + 1)}
+            onMouseOver={() => handleMouseOver(idx + 1)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <Star
+              width={starSize}
+              height={starSize}
+              fill={activeRating > idx ? "#FFCB02" : "var(--color-gray100)"}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
