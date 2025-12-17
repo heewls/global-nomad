6be9bf58e3c0354028a73d278a0e331c8c77e896
heewls <heejin0857@naver.com>
2025-12-18 03:31:24 +0900

    fix: fix modal header structure & props

--- src/components/common/modal/Header.tsx
+++ src/components/common/modal/Header.tsx
@@ -1,21 +1,13 @@
-import clsx from "clsx";
-import Image from "next/image";
-
 interface HeaderProps {
-  image?: string;
-  imageSize?: string;
+  image?: React.ReactNode;
   children: React.ReactNode;
 }
 
-export default function Header({ image, imageSize, children }: HeaderProps) {
+export default function Header({ image, children }: HeaderProps) {
   return (
     <div className="flex flex-col justify-center items-center w-full gap-0.5">
-      {image && (
-        <div className={clsx("relative overflow-hidden", imageSize)}>
-          <Image fill src={image} className="object-cover" alt="notice" />
-        </div>
-      )}
-      <h2 className="w-full break-keep text-center text-gray950 text-16-b sm:text-18-b">
+      {image}
+      <h2 className="w-full text-center text-gray950 text-16-b sm:text-18-b">
         {children}
       </h2>
     </div>
