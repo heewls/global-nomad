348b4371ddc9f33b54866938636b0e566132aedf
heewls <heejin0857@naver.com>
2025-12-22 21:33:38 +0900

    style: add font size & edit responsive css

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -25,7 +25,7 @@ const buttonVariant = {
 const buttonHeight = {
   '29': 'h-[29px]',
   '40': 'h-10',
-  '40-48': 'h-10 md:h-12',
+  '40-48': 'h-10 sm:h-12',
   '50': 'h-12.5',
   '54': 'h-13.5',
 };
@@ -33,7 +33,7 @@ const buttonHeight = {
 const buttonRounded = {
   '8': 'rounded-lg',
   '12': 'rounded-xl',
-  '12-14': 'rounded-xl md:rounded-[14px]',
+  '12-14': 'rounded-xl sm:rounded-[14px]',
   '14': 'rounded-[14px]',
   '16': 'rounded-2xl',
 };
@@ -41,7 +41,8 @@ const buttonRounded = {
 const buttonFontSize = {
   '14-m': 'text-14-m',
   '14-b': 'text-14-b',
-  '14-16-b': 'text-14-b md:text-16-b',
+  '14-16-m': 'text-14-m sm:text-16-m',
+  '14-16-b': 'text-14-b sm:text-16-b',
   '16-b': 'text-16-b',
 };
 
