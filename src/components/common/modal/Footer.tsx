74a6c5d53d976ebf4ccfdff754f5198b5409c662
heewls <heejin0857@naver.com>
2026-01-21 19:44:28 +0900

    style: alert modal button

--- src/components/common/modal/Footer.tsx
+++ src/components/common/modal/Footer.tsx
@@ -1,3 +1,5 @@
 export default function Footer({ children }: { children: React.ReactNode }) {
-  return <div className="flex w-full gap-2 sm:gap-3">{children}</div>;
+  return (
+    <div className="flex w-full justify-center gap-2 sm:gap-3">{children}</div>
+  );
 }
