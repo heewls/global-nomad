9b32e5d764f9f96e0264db7adcd6f337bf350813
heewls <heejin0857@naver.com>
2026-01-29 19:06:19 +0900

    style: edit responsive css

--- src/app/activities/_activities/components/AllActivities.tsx
+++ src/app/activities/_activities/components/AllActivities.tsx
@@ -9,24 +9,24 @@ import useActivities from '../useActivities';
 
 const PAGE_SIZE = 8;
 
-export default function AllActivities({ items }: { items: Activities }) {
-  const { activities, totalCount } = items;
+export default function AllActivities({ all }: { all: Activities }) {
+  const { activities, totalCount } = all;
   const { page: currentPage, updateParams } = useActivities();
 
   const totalPages = Math.ceil(totalCount / PAGE_SIZE);
 
   return (
     <div className="flex flex-col gap-6 sm:gap-7.5">
-      <div className="flex flex-col gap-2.5 sm:gap-4 md:gap-5">
+      <div className="flex flex-col gap-2.5 md:gap-4 lg:gap-5">
         <div className="flex items-center justify-between">
-          <h2 className="text-18-b">모든 체험</h2>
-          <div className="md:hidden">
+          <h2 className="text-18-b md:text-24-b lg:text-32-b">모든 체험</h2>
+          <div className="lg:hidden">
             <Filter />
           </div>
         </div>
         <div className="flex items-center justify-between">
           <Category />
-          <div className="hidden md:flex">
+          <div className="hidden lg:flex">
             <Filter />
           </div>
         </div>
