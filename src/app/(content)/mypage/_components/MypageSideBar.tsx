f74dd75e86d69b03c33fe7123e7b7d2521be2536
heewls <heejin0857@naver.com>
2026-02-19 02:28:44 +0900

    fix: delete edit profile image

--- src/app/(content)/mypage/_components/MypageSideBar.tsx
+++ src/app/(content)/mypage/_components/MypageSideBar.tsx
@@ -10,8 +10,6 @@ import Person from '@/assets/icons/person.svg';
 import Chat from '@/assets/icons/chat.svg';
 import Setting from '@/assets/icons/setting.svg';
 import Calendar from '@/assets/icons/calendar.svg';
-import Edit from '@/assets/icons/edit.svg';
-import FileInput from '@/components/common/input/FileInput';
 
 const DEFAULT_IMAGE = '/icons/defaultProfile.svg';
 
@@ -40,27 +38,16 @@ export default function MypageSideBar() {
   return (
     <div className="border-gray50 h-112.5 w-full shrink-0 rounded-xl border md:h-85.5 md:w-44.5 lg:h-112.5 lg:w-72.5">
       <div className="flex flex-col items-center gap-6 px-3.5 py-6 md:gap-3 lg:gap-6">
-        <FileInput>
-          {({ fileInputRef }) => (
-            <div
-              onClick={() => fileInputRef.current?.click()}
-              className="relative"
-            >
-              <div className="bg-primary100 relative h-30 w-30 cursor-pointer rounded-full md:h-17.5 md:w-17.5 lg:h-30 lg:w-30">
-                <Image
-                  fill
-                  src={user?.profileImageUrl || DEFAULT_IMAGE}
-                  alt="user profile image"
-                  className="object-cover"
-                  sizes="120px"
-                />
-              </div>
-              <div className="bg-gray300 absolute right-0 bottom-1 flex h-7.5 w-7.5 cursor-pointer items-center justify-center rounded-full md:h-6 md:w-6 lg:h-7.5 lg:w-7.5">
-                <Edit className="h-4 w-4 md:h-3 md:w-3 lg:h-4 lg:w-4" />
-              </div>
-            </div>
-          )}
-        </FileInput>
+        <div className="bg-primary100 relative h-30 w-30 rounded-full md:h-17.5 md:w-17.5 lg:h-30 lg:w-30">
+          <Image
+            fill
+            src={user?.profileImageUrl || DEFAULT_IMAGE}
+            alt="user profile image"
+            className="object-cover"
+            sizes="120px"
+          />
+        </div>
+
         <ul className="flex w-full flex-col items-start gap-3.5 md:gap-3 lg:gap-3.5">
           {MYPAGE_MENU.map((menu) => {
             const Icon = menu.icon;
