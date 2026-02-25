de6c09bbfef0409db8ef875d9aeb0ea93e44e29d
heewls <heejin0857@naver.com>
2026-02-25 21:04:26 +0900

    feat: pending & completed button

--- src/app/(content)/mypage/_components/MypageContainer.tsx
+++ src/app/(content)/mypage/_components/MypageContainer.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-import MypageSideBar from './_components/MypageSideBar';
+import MypageSideBar from './MypageSideBar';
 
 export default function MypageContainer({
   children,
