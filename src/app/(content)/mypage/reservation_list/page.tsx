de04fabe16e908d75752e28061accffe5c0532cc
heewls <heejin0857@naver.com>
2026-03-04 16:31:35 +0900

    feat: apply none item

--- src/app/(content)/mypage/reservation_list/page.tsx
+++ src/app/(content)/mypage/reservation_list/page.tsx
@@ -1,5 +1,6 @@
 import ReservationStatus from './_reservation_list/_components/ReservationStatus';
 import ReservationCard from './_reservation_list/_components/ReservationCard';
+import NoneItem from '@/components/NoneItem';
 import { getReservationList } from './_reservation_list/api';
 import { ReservationListParams, TReservationStatus } from '@/types/reservation';
 
@@ -9,27 +10,51 @@ export default async function ReservationList({
   searchParams: Promise<ReservationListParams>;
 }) {
   const params = await searchParams;
+  const currentStatus = params.status;
 
   const reservationResponse = await getReservationList({
-    status: params.status as TReservationStatus,
+    size: 10,
+    status: (currentStatus as TReservationStatus) || undefined,
   });
 
-  const reservations = reservationResponse.reservations;
+  const { reservations, totalCount } = reservationResponse;
 
-  if (!reservations) return null;
+  const isReallyNoneReservations = !currentStatus && totalCount === 0;
+
+  const isInitialEmpty = !currentStatus && reservations.length === 0;
+
+  if (isInitialEmpty) {
+    return (
+      <div className="flex flex-col gap-3.5">
+        <Header />
+        <NoneItem page="reservation-list" lookAround />
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-3.5">
-      <div className="flex flex-col gap-2.5 py-2.5 md:gap-1">
-        <h2 className="text-18-b">예약 내역</h2>
-        <h3 className="text-14-m text-gray500">
-          예약 내역 변경 및 취소할 수 있습니다.
-        </h3>
-      </div>
-      <ReservationStatus />
-      {reservations.map((reservation) => (
-        <ReservationCard key={reservation.id} reservation={reservation} />
-      ))}
+      <Header />
+      {!isReallyNoneReservations && <ReservationStatus />}
+
+      {reservations.length === 0 ? (
+        <NoneItem page="reservation-list" />
+      ) : (
+        reservations.map((reservation) => (
+          <ReservationCard key={reservation.id} reservation={reservation} />
+        ))
+      )}
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="flex flex-col gap-2.5 py-2.5 md:gap-1">
+      <h2 className="text-18-b">예약 내역</h2>
+      <h3 className="text-14-m text-gray500">
+        예약 내역 변경 및 취소할 수 있습니다.
+      </h3>
     </div>
   );
 }
