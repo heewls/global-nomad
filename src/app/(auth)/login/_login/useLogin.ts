7b317be755049a2a96fa98ea427799f75cc2cd7e
heewls <heejin0857@naver.com>
2026-03-07 21:09:57 +0900

    chore: activieis -> root

--- src/app/(auth)/login/_login/useLogin.ts
+++ src/app/(auth)/login/_login/useLogin.ts
@@ -58,7 +58,7 @@ export default function useLogin() {
         setUser(data.user);
         setToken('accessToken', data.accessToken);
         setToken('refreshToken', data.refreshToken);
-        router.push('/activities');
+        router.push('/');
         setIsLoading(false);
       })
       .catch((error) => {
